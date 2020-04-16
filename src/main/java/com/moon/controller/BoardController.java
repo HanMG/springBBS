7125edd4392d7673d8d16f640954870c61803d6d
HanM.G <hanmoongoo@gmail.com>
2020-04-16 11:29:22 +0900

    게시글 조회, 삭제, 수정 추가

--- src/main/java/com/moon/controller/BoardController.java
+++ src/main/java/com/moon/controller/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.moon.domain.BoardVO;
@@ -21,11 +22,13 @@ public class BoardController {
 	@Inject
 	private BoardService service;
 	
+	/* 등록페이지로 이동 */
 	@RequestMapping(value = "/register", method = RequestMethod.GET)
 	public void registGET(BoardVO board, Model model) throws Exception{
 		logger.info("regist GET....");
 	}
 	
+	/* 실제 등록 */
 	@RequestMapping(value = "/register", method = RequestMethod.POST)
 	public String registPOST(BoardVO board, RedirectAttributes rttr) throws Exception{
 		logger.info("regist POST....");
@@ -38,8 +41,43 @@ public class BoardController {
 		return "redirect:/board/listAll";
 	}
 	
+	/* 전체 리스트보기 */
 	@RequestMapping(value = "/listAll",method = RequestMethod.GET)
 	public void listAll(Model model) throws Exception{
 		logger.info("show all list........");
+		model.addAttribute("list", service.listAll());
+	}
+	
+	/* 게시글 읽기 */
+	@RequestMapping(value = "/read", method = RequestMethod.GET)
+	public void read(@RequestParam("bno") int bno, Model model) throws Exception {
+		model.addAttribute(service.read(bno));
+	}
+	
+	/* 게시글 삭제 */
+	@RequestMapping(value = "remove", method = RequestMethod.POST)
+	public String remove(@RequestParam("bno") int bno, RedirectAttributes rttr) throws Exception{
+		service.remove(bno);
+		
+		rttr.addFlashAttribute("msg", "success");
+		
+		return "redirect:/board/listAll";
+	}
+	
+	/* 게시글 수정페이지로 이동 */
+	@RequestMapping(value = "modify", method = RequestMethod.GET)
+	public void modifyGET(int bno, Model model) throws Exception{
+		model.addAttribute(service.read(bno));
+	}
+	
+	/* 실제 수정 */
+	@RequestMapping(value = "modify", method = RequestMethod.POST)
+	public String modifyPOST(BoardVO board, RedirectAttributes rttr) throws Exception{
+		logger.info("modify POST....");
+		
+		service.modify(board);
+		rttr.addFlashAttribute("msg", "success");
+		
+		return "redirect:/board/listAll";
 	}
 }
